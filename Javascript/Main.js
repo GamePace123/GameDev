235155a81e89a41f3be4d54f5ec358b6033142bf
Sebastian <62171533+GamePace123@users.noreply.github.com>
2022-03-30 09:02:02 +0200

    Test 2

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -15,6 +15,11 @@ function Rain(){
     let i = 0;
     while(i < amount){
         let Drop = document.createElement('i');
+        let Size = Math.random() * 5;
+        let PositionX = Math.floor(Math.random() * window.innerWidth);
+
+        Drop.style.width = 0.2 + Size + 'px';
+        Drop.style.left = PositionX + 'px';
         body.appendChild(Drop);
         i++;
     }
