1573f7f8df1da23061148c6998bfda9d692e6d6e
Sebastian <62171533+GamePace123@users.noreply.github.com>
2022-03-30 08:47:16 +0200

    Test Rain Function

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -6,4 +6,16 @@ function LoadComplete()
 {
     var load_screen = document.getElementById("Loadingscreen");
     document.body.removeChild(load_screen);
+    Rain();
+}
+
+function Rain(){
+    let amount = 20;
+    let body = document.querySelector('body');
+    let i = 0;
+    while(i < amount){
+        let Drop = document.createElement('i');
+        body.appendChild(Drop);
+        i++;
+    }
 }
